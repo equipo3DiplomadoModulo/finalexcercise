45c15e266277e6290ce4eeab517ae779330f5877
talos <talos@unam.mx>
2025-02-20 14:41:41 -0600

    Vista de Login (fragment_send_params) ya envía parametros a Loged (LogedActivity)

--- app/src/main/java/com/talos/proyectofinal/SendParmsFragment.kt
+++ app/src/main/java/com/talos/proyectofinal/SendParmsFragment.kt
@@ -6,13 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity.RESULT_OK
 import com.talos.proyectofinal.databinding.FragmentSendParmsBinding
 
 
 class SendParmsFragment : Fragment() {
 
     private lateinit var binding : FragmentSendParmsBinding
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,17 +29,31 @@ class SendParmsFragment : Fragment() {
         // Inflate the layout for this fragment
         binding  = FragmentSendParmsBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
+
         binding.btLogin.setOnClickListener{
-            val intent = Intent(requireContext(), LogedActivity::class.java)
-            startActivity(intent)
+
+            val user = binding.etUser.text
+            val password = binding.etPassword.text
+
+            val loginEmpty : String = getString(R.string.login_empty)
+
+            if (user.toString() == "" || password.toString() == ""){
+                Toast.makeText(requireContext(), loginEmpty, Toast.LENGTH_LONG  ).show()
+            }else{
+                val intent = Intent(requireContext(), LogedActivity::class.java).apply {
+                    putExtra("EXTRA_USER", user.toString())
+                    //Toast.makeText(requireContext(), "name: $name", Toast.LENGTH_LONG).show()
+                }
+                startActivity(intent)
+            }
         }
 
         binding.tvRegister.setOnClickListener {
@@ -48,6 +70,5 @@ class SendParmsFragment : Fragment() {
 
         @JvmStatic
         fun newInstance() = SendParmsFragment()
-
     }
 }
